5eb34aa7566c670579f037ce82cb7ec69128d231
p150 <pavanprakash21@gmail.com>
2017-11-02 22:59:07 +0530

    Add PG Crypto extension to take uuids instead of ids as primary keys

---
+++ db/migrate/20171102172710_add_pg_crypto_uuid_extension.rb
@@ -0,0 +1,5 @@
+class AddPgCryptoUuidExtension < ActiveRecord::Migration[5.1]
+  def change
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102125845) do
+ActiveRecord::Schema.define(version: 20171102172710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pgcrypto"
 
   create_table "doctors", force: :cascade do |t|
     t.string "email", default: "", null: false
