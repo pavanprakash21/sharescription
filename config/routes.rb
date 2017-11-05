8527f47ef2c5c656d570959522755c95d1a567b2
p150 <pavanprakash21@gmail.com>
2017-11-06 03:51:04 +0530

    Add index spec for medical records controller

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,6 @@ Rails.application.routes.draw do
   end
 
   get '/search/suggestions', to: 'search#index'
-  get '/search', to: 'search#search', as: :search
 
   resources :users, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- spec/controllers/medical_records_controller_spec.rb
+++ spec/controllers/medical_records_controller_spec.rb
@@ -1,5 +1,31 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-RSpec.describe MedicalRecordsController, type: :controller do
+describe MedicalRecordsController, type: :controller do
+  describe 'GET #index' do
+    it 'serves success without login' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
 
+    it 'serves success with user login' do
+      user = build :user
+      sign_in(user)
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'serves success with doctor login' do
+      doctor = build :doctor
+      sign_in(doctor)
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'serves success with pharmacist login' do
+      pharmacist = build :pharmacist
+      sign_in(pharmacist)
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
