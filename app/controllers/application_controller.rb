6e46c1cf33956c0c6a3fe72557efbd78545d9fe7
p150 <pavanprakash21@gmail.com>
2017-11-05 17:38:31 +0530

    Add procfile and change the way protect_from_forgery handles the requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception, prepend: true
+  protect_from_forgery prepend: true
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
