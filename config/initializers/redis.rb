7e4c7d35a97afe643bb1849cdb66d12676c8157f
p150 <pavanprakash21@gmail.com>
2017-11-05 17:35:03 +0530

    Change the way redis is instantiated

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,6 +4,12 @@ REDIS_CONFIG = YAML.safe_load(File.open(Rails.root.join('config', 'redis.yml')))
 default = REDIS_CONFIG[:default].symbolize_keys
 config = default.merge(REDIS_CONFIG[Rails.env.to_sym].symbolize_keys) if REDIS_CONFIG[Rails.env.to_sym]
 
-Redis.current = Redis.new(config)
+# Redis.current = Redis.new(config)
+Redis.current = if Rails.env.production?
+                  uri = URI.parse(Rails.application.secrets.redis_url)
+                  Redis.new(url: uri)
+                else
+                  Redis.new(config)
+                end
 
 Redis.current.flushdb if Rails.env == 'test'
