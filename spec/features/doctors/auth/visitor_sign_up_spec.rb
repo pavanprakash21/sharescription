0022d9c45c9549c6d0d4b9ceacf58b08f64da9c4
p150 <pavanprakash21@gmail.com>
2017-11-06 01:49:42 +0530

    Pass doctor and pharmacist sign up spec

---
+++ spec/features/doctors/auth/visitor_sign_up_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+describe Doctor do
+  it 'signs up successfully with valid email and password' do
+    doctor_sign_up('awesome doctor', 'test@example.com', 'please123')
+    expect(page).to have_content 'Log Out'
+  end
+
+  it 'cannot sign up with invalid email address' do
+    doctor_sign_up('awesome doctor', 'bogus', 'please123')
+    expect(page).to have_content 'is invalid'
+  end
+
+  it 'cannot sign up without password' do
+    doctor_sign_up('awesome doctor', 'test@example.com', '')
+    expect(page).to have_content "can't be blank"
+  end
+
+  it 'cannot sign up without name' do
+    doctor_sign_up('', 'test@example.com', 'pleas')
+    expect(page).to have_content "can't be blank"
+  end
+
+  it 'cannot sign up with a short password' do
+    doctor_sign_up('awesome doctor', 'test@example.com', 'pleas')
+    expect(page).to have_content 'is too short'
+  end
+end
